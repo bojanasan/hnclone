f2a5ef9cbc2f3a084890dc8a981eb69169a66956
Bojana <bojanajam@gmail.com>
2012-07-23 15:20:35 -0700

    Make basic user model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+#Returns the full tite on a per-page basis
+
+  def full_title(page_title)
+    base_title = "HNcloneB"
+    if page_title.empty?
+        base_title
+      else
+        "#{base_title} | #{page_title}"
+      end
+    end
+    
 end
